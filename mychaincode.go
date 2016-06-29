d15896725ffc8949abdb068b1fc7506a5b65369f
havinaykumar <havinaykumar@gmail.com>
2016-06-29 15:23:09 +0530

    Updated

--- mychaincode.go
+++ mychaincode.go
@@ -24,6 +24,13 @@ import (
 type SimpleChaincode struct {
 }
 
+type LOC struct {
+   id string
+   buyer string
+   seller string
+   amount int
+}
+
 func main() {
 	err := shim.Start(new(SimpleChaincode))
 	if err != nil {
@@ -41,6 +48,8 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 	if err != nil {
 		return nil, err
 	}
+
+
 
 	return nil, nil
 }
@@ -109,4 +118,32 @@ func (t *SimpleChaincode) read(stub *shim.ChaincodeStub, args []string) ([]byte,
 	}
 
 	return valAsbytes, nil
+}
+
+// read - query function to read key/value pair
+func (t *SimpleChaincode) getlocs(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+
+	var myloc LOC
+	myloc.id = "LOC001"
+	myloc.buyer = "Alex"
+	myloc.seller = "Berney"
+	myloc.amount = 6495
+
+  var loclist [50]LOC
+	loclist[0] = myloc;
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	return []byte(loclist[0]), nil
 }
