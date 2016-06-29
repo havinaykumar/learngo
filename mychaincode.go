4521c4608aeec6ba4c560762d94cd5cb3024d29e
havinaykumar <havinaykumar@gmail.com>
2016-06-29 15:56:41 +0530

    Updated

--- mychaincode.go
+++ mychaincode.go
@@ -97,7 +97,7 @@ func (t *SampleBcCode) read(stub *shim.ChaincodeStub, args []string) ([]byte, er
 	var key, jsonResp string
 	var err error
 
-       var loclist string
+       
        
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
